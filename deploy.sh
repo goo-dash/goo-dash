a2eb590ec0e1e7e5d15d5eab686212777f5598a4
Madimetja Shika <mmjshika@gmail.com>
2017-10-09 11:38:53 +0200

    Updated the name of the deploy files

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Trigger deployment
-echo 'Starting deployment...'
-curl -s 'https://forge.laravel.com/servers/95687/sites/413691/deploy/http?token=$DEPLOY_TOKEN';
-echo 'Deployment triggered!'
