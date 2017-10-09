25375e51733024af3fefb213cc1f236b499306c2
Madimetja Shika <mmjshika@gmail.com>
2017-10-09 14:07:35 +0200

    Removed links to materialize...

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,8 +10,6 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'A consolidated list of Google Consoles/Dashboards' }
     ],
     link: [
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' },
-      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css' },
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
@@ -42,7 +40,7 @@ module.exports = {
 
   css: [
     // 'materialize-css',
-    // '@/node_modules/materialize-css/dist/css/materialize.css'
+    '@/node_modules/materialize-css/dist/css/materialize.css'
   ],
 
   modules: [
