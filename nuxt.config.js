f7832fd9839fae0328dcf77bee797b9476ef4785
Madimetja Shika <mmjshika@gmail.com>
2017-10-10 13:13:59 +0200

    Updated Platformm Title

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'goo-dash',
+    title: 'Goo(gle) Dash(board)',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
