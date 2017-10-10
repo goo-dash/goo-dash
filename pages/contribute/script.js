d14c1ff5ddd8332935336e4a983b80ae4306b804
Makhabane Christopher Mipi <christopher.mipi@gmail.com>
2017-10-10 13:47:41 +0200

    validations work

--- pages/contribute/script.js
+++ pages/contribute/script.js
@@ -20,25 +20,61 @@ export default {
     }
   },
   methods: {
-    validateBeforeSubmit(e) {
+    validateBeforeSubmit(link) {
+      let router = this.$router;
+      let validator = this.$validator;
       this.$validator.validateAll().then(x => {
-        console.log(x)
+        console.log(this.$validator.errors.items);
+        console.log(x);
+        if(!x){
+          return;
+        }
+        else {
+          let url = 'https://us-central1-goodash-8856b.cloudfunctions.net/api/links/';
+          axios({
+            method: 'post',
+            url: url,
+            data: link,
+          }).then(function(response){
+            cosole.log(response);
+            if(response.status == 201) {
+              router.push('/');
+              Materialize.toast('Thanks for contributing', 4000);
+            }
+            else if(response.status == 400){
+              const error = {
+                field:"url",
+                id:"_jfp0gbni4",
+                msg:"The url already exists.",
+                rule:"required",
+                scope:null
+              }
+              validator.errors.push(error);
+            }
+            else {
+              Materialize.toast('Something wrong happened, please try again later.', 4000);
+            }
+          }).catch(function(err) {
+            console.log(err);
+          });
+        }
       }).catch(e => {
         console.log(e)
       })
     },
     submit(link) {
+      console.log('posting');
       let url = 'https://us-central1-goodash-8856b.cloudfunctions.net/api/links/';
-      Materialize.toast('Thanks for contributing', 4000);
-      // axios({
-      //   method: 'post',
-      //   url: url,
-      //   data: link,
-      // }).then(function(response){
-      //   this.$router.push('/');
-      // }).catch(function(err) {
-      //   console.log(err);
-      // });
+      axios({
+        method: 'post',
+        url: url,
+        data: link,
+      }).then(function(response){
+        this.$router.push('/');
+        Materialize.toast('Thanks for contributing', 4000);
+      }).catch(function(err) {
+        console.log(err);
+      });
     },
     imageUpload (event) {
       let input = event.target;
--- plugins/vee-validate.js
+++ plugins/vee-validate.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import VeeValidate from 'vee-validate'
 
-Vue.use(VeeValidate, {
+const config = {
+  errorBagName: 'errors', // change if property conflicts.
+  fieldsBagName: 'fields',
+  events: 'input|blur',
   inject: true
-})
+}
+
+Vue.use(VeeValidate, config)
